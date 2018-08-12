a4d98353419987cf2357552ae1f5e71a35974901
Natty Davis <natty.davis79@gmail.com>
2018-08-12 13:23:34 -0400

    initial commit
    lets see if it works

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Aggregate extends Component {
+  render () {
+    return(
+        <div style={{width:'40%', display:'inline-block'}}>
+          <h2 style={{color:"blue"}}> Number text</h2>
+        </div>
+      )
+  }
+}
+
+class Filter extends Component {
+  render () {
+    return(
+        <div>
+          <img/>
+          <input type='text' ></input>
+        </div>
+      )
+  }
+}
+
+class PlayList extends Component {
+  render () {
+    return(
+        <div style={{width:'33%', display:'inline-block'}}>
+          <img/>
+          <h3>Playlist</h3>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+          </ul>
+        </div>
+      )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="Big">Title</h1>
+        <Aggregate/>
+        <Aggregate/>
+        <Filter/>
+        <PlayList/>
+        <PlayList/>
+        <PlayList/>
+        <PlayList/>
+      </div>
+    );
+  }
+}
+
+export default App;
